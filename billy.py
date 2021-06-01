3332d70b935d4f05e11c187c0f85d73c32f35ec9
linas <linasalekne@gmail.com>
2021-06-01 17:31:42 -0400

    added sbuway

--- billy.py
+++ billy.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands 
 from dotenv import load_dotenv
 from music_controller import music_controller
-from idk import get_newest, get_top 
+from idk import get_newest, get_top, get_topS, get_newestS
 
 load_dotenv() 
 TOKEN = 'ODQ5MTIzMzg4NTY2Nzk4MzY4.YLWlxw.pyJX_ePIxIP9ynOAWQHijndJ450'
@@ -35,7 +35,7 @@ async def play(ctx, arg):
 
 
 @client.command()
-async def bill(ctx, arg):
+async def sam(ctx, arg):
         if (arg == 'top'): 
             content = await get_top() 
             print(content)
@@ -46,11 +46,30 @@ async def bill(ctx, arg):
 
         elif (arg == 'new'): 
             content = await get_newest()  
-            
-            embed=discord.Embed(title=content[0][1], url=content[0][0], description="This is an embed that will show how to build an embed and the different components", image=content[0][0], color=0xFF5733)
-            embed.set_image(url=content[0][0])
+            if (content[0][0].endswith('jpg') or content[0][0].endswith('png')): 
+                embed=discord.Embed(title=content[0][1], url=content[0][0], description='u/' + content[0][2], color=0xFF5733)
+                embed.set_image(url=content[0][0])
+            else: 
+                embed=discord.Embed(title=content[0][1], description=content[0][0], color=0xFF5733)
             await ctx.send(embed=embed)       
 
+@client.command()
+async def subway(ctx, arg):
+        if (arg == 'top'): 
+            content = await get_topS() 
+            print(content)
+            print('i got here')
+            embed=discord.Embed(title=content[0][1], url=content[0][0], description='u/' + content[0][2], color=0xFF5733)
+            embed.set_image(url=content[0][0])
+            await ctx.send(embed=embed)           
 
+        elif (arg == 'new'): 
+            content = await get_newestS()  
+            if (content[0][0].endswith('jpg') or content[0][0].endswith('png')): 
+                embed=discord.Embed(title=content[0][1], url=content[0][0], description='u/' + content[0][2], color=0xFF5733)
+                embed.set_image(url=content[0][0])
+            else: 
+                embed=discord.Embed(title=content[0][1], description=content[0][0], color=0xFF5733)
+            await ctx.send(embed=embed)     
 
 client.run(TOKEN)
--- idk.py
+++ idk.py
@@ -9,23 +9,24 @@ reddit = asyncpraw.Reddit(client_id='9bfw125sbnQY9Q', client_secret='dX7Xnh1xDJV
 #for post in hot_posts: 
 #    print(post.url)
 
-post_info = []
-async def get_newest():
 
+async def get_newest():
+    post_info = []
     newest1 = await reddit.subreddit('SamsungGirlr34')
     newest = newest1.new(limit=1)
     async for post in newest: 
         print(post.url)
         if (post.url.endswith('jpg') or post.url.endswith('png')): 
             print(post.url)
-            post_info.append([post.url, post.title])
+            post_info.append([post.url, post.title, post.author.name])
             return post_info 
         else: 
-            post_info.append([post.body, post.title])
+            post_info.append([post.selftext, post.title, post.author.name])
             return post_info 
 
 
 async def get_top(): 
+    post_info = []
     current_top1 = await reddit.subreddit('SamsungGirlr34')
     current_top = current_top1.top(limit=1)
    #subreddit = await reddit.subreddit("SamsungGirlr34")
@@ -37,10 +38,39 @@ async def get_top():
             post_info.append([post.url, post.title, post.author.name])
             return post_info 
         else: 
-            post_info.append([post.body, post.title, post.author.name])
+            post_info.append([post.selftext, post.title, post.author.name])
             return post_info 
          
+async def get_newestS():
+    post_info = []
+    newest1 = await reddit.subreddit('SubwayHentai')
+    newest = newest1.new(limit=1)
+    async for post in newest: 
+        print(post.url)
+        if (post.url.endswith('jpg') or post.url.endswith('png')): 
+            print(post.url)
+            post_info.append([post.url, post.title, post.author.name])
+            return post_info 
+        else: 
+            post_info.append([post.selftext, post.title, post.author.name])
+            return post_info 
 
+
+async def get_topS(): 
+    post_info = []
+    current_top1 = await reddit.subreddit('SubwayHentai')
+    current_top = current_top1.top(limit=1)
+   #subreddit = await reddit.subreddit("SamsungGirlr34")
+    async for post in current_top: 
+        print(post.url)
+        
+        if (post.url.endswith('jpg') or post.url.endswith('png')): 
+            print(post.url)
+            post_info.append([post.url, post.title, post.author.name])
+            return post_info 
+        else: 
+            post_info.append([post.selftext, post.title, post.author.name])
+            return post_info 
 #loop = asyncio.get_event_loop()
 #loop.run_until_complete(get_top())
 
